27106c6c8eec1aba60f3b263c2b32b0bf2bacf14
Agung Pratama <agung@sembly.com>
2023-12-25 14:33:11 +0700

    Time: 8 ms (40.32%), Space: 4.6 MB (81.88%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.go
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func hasCycle(head *ListNode) bool {
+    if head == nil || head.Next == nil {
+        return false
+    }
+    // using hare and tortoise algo
+    fast, slow := head, head
+
+    for fast != nil && fast.Next != nil {
+        slow = slow.Next
+        fast = fast.Next.Next
+
+        if slow == fast {
+            return true
+        }
+    }
+
+    return false
+}
