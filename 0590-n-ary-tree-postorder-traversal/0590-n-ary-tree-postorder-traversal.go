0e5985df3d9fc1474cc44c695d9383720a9e6f2c
Agung Pratama <agung@sembly.com>
2023-12-27 13:16:13 +0700

    Time: 3 ms (72.6%), Space: 4.3 MB (52.05%) - LeetHub

--- 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.go
+++ 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.go
@@ -7,13 +7,48 @@
  */
 
 func postorder(root *Node) []int {
+    // //1. recursive approach
+    // if root == nil {
+    //     return nil
+    // }
+    // var res []int
+    // for _, child:= range root.Children{
+    //     res = append(res, postorder(child)...)
+    // }
+    // res = append(res, root.Val)
+    // return res
+
+    // 2. iterative approach
+    return postorderIterative(root)
+}
+
+func postorderIterative(root *Node) []int {
     if root == nil {
         return nil
     }
+
     var res []int
-    for _, child:= range root.Children{
-        res = append(res, postorder(child)...)
+    stack := make([]*Node, 0)
+    stack = append(stack, root)
+    stackVal := make([]int, 0)
+    for len(stack) > 0 {
+        node := stack[len(stack)-1] //pop top item
+        stack = stack[0:len(stack)-1]
+
+
+        stackVal = append(stackVal, node.Val)
+
+        for _, child := range node.Children{
+            if child != nil {
+                stack = append(stack, child)
+            }
+        }
+
     }
-    res = append(res, root.Val)
+    for len(stackVal) > 0 {
+        pop := stackVal[len(stackVal)-1]
+        stackVal = stackVal[0:len(stackVal)-1]
+        res = append(res, pop)
+    }
     return res
 }
