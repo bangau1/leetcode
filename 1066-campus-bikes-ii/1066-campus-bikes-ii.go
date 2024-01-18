b14f34cc02583a4e60c1b9e422af8464b9189c14
Agung Pratama <agung@sembly.com>
2024-01-18 16:54:47 +0700

    Time: 2 ms (95.31%), Space: 2.3 MB (64.06%) - LeetHub

--- 1066-campus-bikes-ii/1066-campus-bikes-ii.go
+++ 1066-campus-bikes-ii/1066-campus-bikes-ii.go
@@ -23,6 +23,47 @@ func calculateMinSum(worker int, workers [][]int, bikes [][]int, bikeFlag int, c
     }
 }
 
+// kernighan algo
+func countSetBit(n int) int {
+    count := 0
+    for n != 0 {
+        n &= (n-1)
+        count++
+    }
+    return count
+}
+func dpSolution(workers [][]int, bikes [][]int) int {
+
+    dp := make([]int, 1 << len(bikes))
+    for i:=0;i<len(dp);i++{
+        dp[i] = math.MaxInt
+    }
+    dp[0] = 0 // no bike being assigned, so the sum is 0
+    minSolution := math.MaxInt
+
+    for mask := 0;mask < 1 << len(bikes);mask++{
+        nextWorkerIdx := countSetBit(mask)
+
+        if nextWorkerIdx >= len(workers) {
+            minSolution = min(minSolution, dp[mask])
+            continue
+        }
+
+        for i:=0;i<len(bikes);i++{
+            
+            if mask & (1 << i) == 0 {
+                newMask := mask | 1 << i
+                dist := dist(workers[nextWorkerIdx], bikes[i])
+                dp[newMask] = min(dp[newMask], dp[mask] + dist)
+            }
+        }
+
+
+    }
+    return minSolution
+
+}
+
 
 func dist(workerPos []int, bikePos []int) int {
     return abs(workerPos[0]-bikePos[0]) + abs(workerPos[1] - bikePos[1])
@@ -36,8 +77,10 @@ func abs(a int) int {
 }
 
 func assignBikes(workers [][]int, bikes [][]int) int {
-    var minSum = math.MaxInt
-    calculateMinSum(0, workers, bikes, 0, 0, &minSum)
-    return minSum
+    // var minSum = math.MaxInt
+    // calculateMinSum(0, workers, bikes, 0, 0, &minSum)
+    // return minSum
+
+    return dpSolution(workers, bikes)
 }
 
