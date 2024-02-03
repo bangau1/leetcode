2600ebb9fe2d6d76b4a19e4cd7cf683d01566a38
Agung Pratama <agung@sembly.com>
2024-02-03 11:13:20 +0700

    Time: 105 ms (75.69%), Space: 10.8 MB (33.09%) - LeetHub

--- 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.go
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.go
@@ -9,14 +9,15 @@ func minOperations(nums []int) int {
         if count == 1 {
             return -1
         }
-        // fmt.Println(num, count)
-        if (count %3) % 2 == 0 {
-            total += count/3 + (count %3)/2
-            // fmt.Println(num, "total", count/3 + (count %3)/2)
-        }else {
-            total += count/3 - 1 + (count - 3 * (count/3 - 1)) /2  
-            // fmt.Println(num, "total", count/3 - 1 + (count - (count/3 - 1)) /2  )
+        switch count % 3 {
+            case 1 :
+                total += count/3-1 + 2
+            case 2 :
+                total+= count/3 + 1
+            default:
+                total += count/3
         }
+
     }
 
     return total
