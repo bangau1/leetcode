651fc3235086887bf6cbea736b6b2ff49556792c
Agung Pratama <agung@sembly.com>
2024-02-05 09:13:21 +0700

    Time: 3 ms (96.73%), Space: 5.3 MB (68.85%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.go
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.go
@@ -1,12 +1,12 @@
 func firstUniqChar(s string) int {
-    counter := make(map[byte]int)
+    counter := make([]int, 26)
 
     for i:=0;i<len(s);i++{
-        counter[s[i]]++
+        counter[int(s[i]-'a')]++
     }
 
     for i:=0;i<len(s);i++{
-        if counter[s[i]] == 1 {
+        if counter[int(s[i]-'a')] == 1 {
             return i
         }
     }
