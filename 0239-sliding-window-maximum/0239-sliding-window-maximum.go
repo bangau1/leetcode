f70ed510b59d2fab344fbda6744175e14820597a
Agung Pratama <agung@sembly.com>
2024-02-05 16:21:33 +0700

    Time: 194 ms (81.22%), Space: 10.6 MB (46.07%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.go
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.go
@@ -1,13 +1,3 @@
-type data struct {
-    val, index int
-}
-func printVal(arrs []data) {
-    res := make([]int, 0)
-    for _, datum := range arrs{
-        res = append(res, datum.val)
-    }
-    fmt.Println(res)
-}
 func maxSlidingWindow(nums []int, k int) []int {
     if k == 1 {
         return nums
@@ -17,28 +7,29 @@ func maxSlidingWindow(nums []int, k int) []int {
         panic("unexpected k > n")
     }
 
-    queue := make([]data, 0)
+    queue := make([]int, 0)
 
     for i:=0;i<k;i++{
-        for len(queue) > 0 && nums[i] > queue[len(queue)-1].val {
+        for len(queue) > 0 && nums[i] > nums[queue[len(queue)-1]] {
             queue = queue[0:len(queue)-1]
         }
-        queue = append(queue, data{nums[i], i})
+        queue = append(queue, i)
     }
     res := make([]int, 0)
-    res = append(res, queue[0].val)
+    res = append(res, nums[queue[0]])
     
+    start := 1
     for i:=k;i<n;i++{
-        start:= i-k+1
-        for len(queue) > 0 && queue[0].index < start {
+        start = i - k + 1
+        for len(queue) > 0 && queue[0] < start {
             queue = queue[1:]
         }
-        for len(queue) > 0 && nums[i] > queue[len(queue)-1].val {
+        for len(queue) > 0 && nums[i] > nums[queue[len(queue)-1]] {
             queue = queue[0:len(queue)-1]
         }
-        queue = append(queue, data{nums[i], i})
+        queue = append(queue, i)
         
-        res = append(res, queue[0].val)
+        res = append(res, nums[queue[0]])
     }
     return res
 }
