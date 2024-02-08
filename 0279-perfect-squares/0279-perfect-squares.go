2549af95ecf2ecd3e34522a86e369ec737cdba80
Agung Pratama <agung@sembly.com>
2024-02-08 09:12:06 +0700

    Time: 27 ms (64.65%), Space: 6.1 MB (74.75%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.go
@@ -0,0 +1,39 @@
+func numSquares(n int) int {
+    dp := make([]int, n+1)
+    for i:=0;i<=n;i++{
+        dp[i] = -1
+    }
+    dp[0] = 0
+    
+    psq := make([]int, 0)
+    for i:=1;i<=100;i++{
+        sq := i*i
+        psq = append(psq, sq)
+        if sq <= n {
+            dp[sq] = 1
+        }
+        if sq == n {
+            return 1 
+        }
+    }
+
+    // dp[i] = min(dp[i-prevSq] + 1)
+
+    for i:=1;i<=n;i++{
+        if dp[i] == 1 {
+            continue
+        }
+
+        // looking the greatest prevSquare < i
+        upper := sort.Search(len(psq), func(idx int) bool {
+            return psq[idx] > i
+        })
+        res := math.MaxInt
+        for ii:= upper-1;ii>=0;ii--{
+            res = min(res, 1 + dp[i-psq[ii]])
+        }
+        dp[i] = res
+    }
+
+    return dp[n]
+}
