95af71f729bee75683133cbdeba774746b0b1c6b
Agung Pratama <agung@sembly.com>
2024-02-10 14:13:54 +0700

    Time: 16 ms (95.78%), Space: 2.4 MB (96.99%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.go
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.go
@@ -12,104 +12,21 @@ func reverse(s string) string {
     return string(res)
 }
 
-// var charPos [26][]int
-var dp [][]int
-func originalSolution(s string) int {
-    n := len(s)
-    maxLen := 1
-    // charPos = [26][]int{}
-    dp = make([][]int,n)
-    for i:=0;i<n;i++{
-        dp[i] = make([]int, n)
-        for ii:=0;ii<n;ii++{
-            dp[i][ii] = -1
-        }
-        // dp[i][i] = 1
-    }
-
-    // for i:=0;i<n;i++{
-    //     letter := int(s[i]-'a')
-    //     charPos[letter] = append(charPos[letter], i)
-    // }
-    for i:=0;i<n;i++{
-        l, r := i-1, i + 1
-        maxLen = max(maxLen, dfs(l, r, &s) + 1)
-        
-        l, r = i-1, i
-        maxLen = max(maxLen, dfs(l, r, &s))
-    }
-
-    return maxLen
-}
-
-
-func dfs(l,r int, sPtr *string) int {
-    if l == r {
-        panic("unexpected")
-    }
-    s := *sPtr
-    n := len(s)
-    if l < 0 || r >= n {
-        return 0
-    }
-    if dp[l][r] != -1 {
-        return dp[l][r]
-    }
-
-    result := 0
-    if s[l] == s[r] {
-        result = 2 + dfs(l-1, r+1, sPtr)
-    }else{
-        result = max(dfs(l-1, r, sPtr), dfs(l, r+1, sPtr))
-        // choose nothing
-        // maxi = dfs(l-1, r+1, sPtr)
-        // maxi = max(maxi, dfs(l-1))
-        
-        // choose left
-        // letter := s[l]-'a'
-        // leftPos := charPos[letter]
-        // idx := sort.SearchInts(leftPos, r+1)
-        // if idx < len(leftPos) && leftPos[idx] > r {
-        //     if leftPos[idx-1] >= r {
-        //         panic("unexpected error")
-        //     }
-        //     maxi = max(maxi, dfs(l-1, leftPos[idx]+1, sPtr) + 2 )
-        // }
-
-        // chose right
-        // letter = s[r]-'a'
-        // rightPos := charPos[letter]
-        // idx = sort.SearchInts(rightPos, l-1)
-        // if rightPos[idx] >= 0 && rightPos[idx] < l {
-        //     if rightPos[idx+1] <= l {
-        //         panic("unexpected error")
-        //     }
-        //     maxi = max(maxi, dfs(rightPos[idx]-1, r+1, sPtr) + 2 )
-        // }
-
-        // result = maxi
-    }
-
-    dp[l][r] = max(dp[l][r], result)
-    return dp[l][r]
-}
-
 func longestCommonSubsequence(text1 string, text2 string) int {
     m, n := len(text1), len(text2)
-    dp := make([][]int, m + 1)
-    for i:=0;i<=m;i++{
-        dp[i] = make([]int, n + 1)
-    }
+    curr := make([]int, n+1)
+    prev := make([]int, n+1)
 
     for i:=1;i<=m;i++{
+        curr, prev = prev, curr
         for ii:=1;ii<=n;ii++{
             if text1[i-1] == text2[ii-1] {
-                dp[i][ii] = dp[i-1][ii-1] + 1
+                curr[ii] = prev[ii-1] + 1
             }else{
-                dp[i][ii] = max(dp[i-1][ii], dp[i][ii-1])
+                curr[ii] = max(prev[ii], curr[ii-1])
             }
         }
     }
 
-    return dp[m][n]
+    return curr[n]
 }
