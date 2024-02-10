bc693919bdc5574707daad226931db671c26cb45
Agung Pratama <agung@sembly.com>
2024-02-10 14:10:41 +0700

    Time: 51 ms (31.93%), Space: 18.9 MB (62.65%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.go
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.go
@@ -1,6 +1,20 @@
+func longestPalindromeSubseq(s string) int {
+    return longestCommonSubsequence(s, reverse(s))
+}
+
+func reverse(s string) string {
+    n := len(s)
+    res := make([]byte, n)
+    
+    for i:=0;i<n;i++{
+        res[i] = s[n-1-i]
+    }
+    return string(res)
+}
+
 // var charPos [26][]int
 var dp [][]int
-func longestPalindromeSubseq(s string) int {
+func originalSolution(s string) int {
     n := len(s)
     maxLen := 1
     // charPos = [26][]int{}
@@ -27,6 +41,7 @@ func longestPalindromeSubseq(s string) int {
 
     return maxLen
 }
+
 
 func dfs(l,r int, sPtr *string) int {
     if l == r {
@@ -77,4 +92,24 @@ func dfs(l,r int, sPtr *string) int {
 
     dp[l][r] = max(dp[l][r], result)
     return dp[l][r]
+}
+
+func longestCommonSubsequence(text1 string, text2 string) int {
+    m, n := len(text1), len(text2)
+    dp := make([][]int, m + 1)
+    for i:=0;i<=m;i++{
+        dp[i] = make([]int, n + 1)
+    }
+
+    for i:=1;i<=m;i++{
+        for ii:=1;ii<=n;ii++{
+            if text1[i-1] == text2[ii-1] {
+                dp[i][ii] = dp[i-1][ii-1] + 1
+            }else{
+                dp[i][ii] = max(dp[i-1][ii], dp[i][ii-1])
+            }
+        }
+    }
+
+    return dp[m][n]
 }
