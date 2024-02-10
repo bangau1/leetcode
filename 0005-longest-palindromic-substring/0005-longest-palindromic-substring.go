af69a8177622c65ac89bcd7bfe1820c6e8c2d3ce
Agung Pratama <agung@sembly.com>
2024-02-10 12:31:37 +0700

    Time: 5 ms (73.46%), Space: 2.5 MB (54.84%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.go
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.go
@@ -4,32 +4,30 @@ func longestPalindrome(s string) string {
 
     for i:=1;i<n;i++{
         l, r := i-1, i+1
-
-        for l >= 0 && r < n {
-            if s[l] != s[r] {
-                l = l+1
-                r = r - 1
-                if len(res) < (r-l+1){
-                    res = s[l:r+1]
-                }
-                break
-            }
+        found := false
+        for l >= 0 && r < n && s[l] == s[r] {
             l--
             r++
+            found = true
+        }
+        l = l+1
+        r = r-1
+        if found && len(res) < r-l+1{
+            res = s[l:r+1]
         }
 
+
         l, r = i-1, i
-        for l >= 0 && r < n {
-            if s[l] != s[r] {
-                l = l+1
-                r = r - 1
-                if len(res) < (r-l+1){
-                    res = s[l:r+1]
-                }
-                break
-            }
+        found = false
+        for l >= 0 && r < n && s[l] == s[r] {
             l--
             r++
+            found = true
+        }
+        l = l+1
+        r = r-1
+        if found && len(res) < r-l+1{
+            res = s[l:r+1]
         }
     }
 
