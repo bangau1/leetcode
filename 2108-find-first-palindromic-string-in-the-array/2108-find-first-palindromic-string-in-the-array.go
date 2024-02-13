b4f22a4446d8eb45a6c80d70c946821dbbb57179
Agung Pratama <agung@sembly.com>
2024-02-13 08:17:48 +0700

    Time: 16 ms (49.12%), Space: 6.9 MB (35.96%) - LeetHub

---
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.go
@@ -0,0 +1,33 @@
+func firstPalindrome(words []string) string {
+    for _, word := range words {
+        if isPalindrome(word) {
+            return word
+        }
+    }
+    return ""
+}
+
+func isPalindrome(s string) bool {
+    n := len(s)
+    if n == 1 {
+        return true
+    }
+    if n == 2 {
+        return s[0] == s[1]
+    }
+    var l, r int
+    if n % 2 == 0 {
+        l, r = n/2 -1, n/2
+    }else{
+        l, r = n/2-1, n/2 + 1
+    }
+
+    for l >= 0 && r < n {
+        if s[l] != s[r] {
+            return false
+        }
+        l--
+        r++
+    }
+    return true
+}
