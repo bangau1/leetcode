c036f1137226da64b5bac210fbfc798df8703ed6
Agung Pratama <agung@sembly.com>
2024-02-15 21:32:26 +0700

    Time: 322 ms (18.69%), Space: 9.4 MB (65.18%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.go
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.go
@@ -2,7 +2,8 @@ func maxSlidingWindow(nums []int, k int) []int {
     if k == 1 {
         return nums
     }
-    var res []int
+    n := len(nums)
+    res := make([]int, n - k + 1)
     deque := list.New()
 
     for i:=0;i<k;i++{
@@ -11,16 +12,23 @@ func maxSlidingWindow(nums []int, k int) []int {
         }
         deque.PushBack(i)
     }
-
-    res = append(res, nums[deque.Front().Value.(int)])
-    n := len(nums)
+    idx := 0
+    res[idx] = nums[deque.Front().Value.(int)]
+    idx++
     
+    var start int
     for i:=k;i<n;i++{
         for deque.Len() > 0 && nums[i] >= nums[deque.Back().Value.(int)] {
             deque.Remove(deque.Back())
         }
+        // valid range: start=i-k+1 end=i
+        start = i - k + 1
+        for deque.Len() > 0 && deque.Front().Value.(int) < start {
+            deque.Remove(deque.Front())
+        }
         deque.PushBack(i)
-        res = append(res, nums[deque.Front().Value.(int)])
+        res[idx] = nums[deque.Front().Value.(int)]
+        idx++
     }
     return res
 }
