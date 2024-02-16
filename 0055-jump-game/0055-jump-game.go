82f14a163dd8b723729f401ae1207d7e2a20adb2
Agung Pratama <agung@sembly.com>
2024-02-16 13:55:34 +0700

    Time: 284 ms (16.56%), Space: 8.7 MB (7.86%) - LeetHub

--- 0055-jump-game/0055-jump-game.go
+++ 0055-jump-game/0055-jump-game.go
@@ -7,14 +7,31 @@ func canJump(nums []int) bool {
         dp[i] = -1
     }
     dp[n-1] = n-1
-    for i:=n-2;i>=0;i--{
-        if i + nums[i] >= n-1{
-            dp[i] = n-1
+    dfs(0, dp, nums)
+    return dp[0] == n -1
+}
+
+func dfs(idx int, dp []int, nums []int){
+    n := len(nums)
+    if idx == n-1 {
+        dp[idx] = n-1
+        return
+    }
+    if nums[idx] == 0{
+        dp[idx] = idx
+        return
+    }
+    if dp[idx] != -1 {
+        return
+    }
+
+    for jump:=nums[idx];jump >=1;jump--{
+        if idx + jump >= n-1{
+            dp[idx] = n-1
+            break
         }else{
-            for jump:=1;jump<=nums[i];jump++{
-                dp[i] = max(dp[i], dp[i+jump])
-            }
+            dfs(idx+jump, dp, nums)
+            dp[idx] = max(dp[idx], dp[idx+jump])
         }
     }
-    return dp[0] == n-1
 }
