1909f3df5705570457ebc752f3d65a4b8edb6d31
Agung Pratama <agung@sembly.com>
2024-02-16 12:53:21 +0700

    Time: 30 ms (42.05%), Space: 8.1 MB (20.91%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.go
@@ -0,0 +1,27 @@
+func rotate(nums []int, k int)  {
+    c := 0
+    n := len(nums)
+    i := 0
+    for c < n {
+        c += doRotate(i, nums, k)
+        i++
+    }
+    
+}
+
+func doRotate(idx int, nums []int, k int) int {
+    n := len(nums)
+    c := 1
+    prev := idx
+    next := (prev + k ) % n
+    var tmp int
+    nums[next], tmp = nums[prev], nums[next]
+        
+    for next != idx {
+        prev = next
+        next = (prev + k ) % n
+        nums[next], tmp = tmp, nums[next]
+        c++
+    }
+    return c
+}
