1ba9d10adcaaa7bf58484e693dba7fb6c4e5213c
Agung Pratama <agung@sembly.com>
2024-02-21 22:30:07 +0700

    Time: 6 ms (36.84%), Space: 4.9 MB (18.42%) - LeetHub

--- 0243-shortest-word-distance/0243-shortest-word-distance.go
+++ 0243-shortest-word-distance/0243-shortest-word-distance.go
@@ -7,33 +7,50 @@ func shortestDistance(wordsDict []string, word1 string, word2 string) int {
     // 
 
     // naive solution
-    a := make([]int, 0)
-    b := make([]int, 0)
-    for i, word := range wordsDict {
-        if word == word1 {
-            a = append(a, i)
-        }else if word == word2{
-            b = append(b, i)
-        }
-    }
-    iA, iB := 0, 0
-    minRes := abs(a[iA]-b[iB])
+    // a := make([]int, 0)
+    // b := make([]int, 0)
+    // for i, word := range wordsDict {
+    //     if word == word1 {
+    //         a = append(a, i)
+    //     }else if word == word2{
+    //         b = append(b, i)
+    //     }
+    // }
+    // iA, iB := 0, 0
+    // minRes := abs(a[iA]-b[iB])
     
-    var diff int
-    for iA < len(a) && iB < len(b) {
-        diff = abs(a[iA]-b[iB])
+    // var diff int
+    // for iA < len(a) && iB < len(b) {
+    //     diff = abs(a[iA]-b[iB])
 
-        for iA < len(a) && iB < len(b) && a[iA] < b[iB] {
-            diff = min(diff, b[iB] - a[iA])
-            iA++
-        } 
+    //     for iA < len(a) && iB < len(b) && a[iA] < b[iB] {
+    //         diff = min(diff, b[iB] - a[iA])
+    //         iA++
+    //     } 
 
-        for iA < len(a) && iB < len(b) && b[iB] < a[iA] {
-            diff = min(diff, a[iA] - b[iB])
-            iB++
+    //     for iA < len(a) && iB < len(b) && b[iB] < a[iA] {
+    //         diff = min(diff, a[iA] - b[iB])
+    //         iB++
+    //     }
+    //     minRes = min(minRes, diff)
+    // }
+
+    // optimize one pass solution
+    var minRes = math.MaxInt
+    i, j := -1, -1
+
+    for idx, word := range wordsDict {
+        if word == word1{
+            i = idx
+        }else if word == word2 {
+            j = idx
         }
-        minRes = min(minRes, diff)
+
+        if i != -1 && j != -1 {
+            minRes = min(minRes, abs(i-j))
+        }
     }
+
     return minRes
 }
 
