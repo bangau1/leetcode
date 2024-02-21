b5849b66328fedd727b952e92d7db1715a3d7058
Agung Pratama <agung@sembly.com>
2024-02-21 22:26:07 +0700

    Time: 5 ms (71.05%), Space: 4.9 MB (36.84%) - LeetHub

---
+++ 0243-shortest-word-distance/0243-shortest-word-distance.go
@@ -0,0 +1,45 @@
+func shortestDistance(wordsDict []string, word1 string, word2 string) int {
+    // ["practice", "makes", "perfect", "coding", "makes"], word1 = "makes", word2 = "coding"
+    // makes = [1,4]
+    // coding = [3,6]
+    // 1,3 = 2
+    // 3,4 = 1
+    // 
+
+    // naive solution
+    a := make([]int, 0)
+    b := make([]int, 0)
+    for i, word := range wordsDict {
+        if word == word1 {
+            a = append(a, i)
+        }else if word == word2{
+            b = append(b, i)
+        }
+    }
+    iA, iB := 0, 0
+    minRes := abs(a[iA]-b[iB])
+    
+    var diff int
+    for iA < len(a) && iB < len(b) {
+        diff = abs(a[iA]-b[iB])
+
+        for iA < len(a) && iB < len(b) && a[iA] < b[iB] {
+            diff = min(diff, b[iB] - a[iA])
+            iA++
+        } 
+
+        for iA < len(a) && iB < len(b) && b[iB] < a[iA] {
+            diff = min(diff, a[iA] - b[iB])
+            iB++
+        }
+        minRes = min(minRes, diff)
+    }
+    return minRes
+}
+
+func abs(a int) int {
+    if a < 0 {
+        return -a
+    }
+    return a
+}
