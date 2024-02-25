e0405a8da856c4ddd699e719f78a8d44b96efc6b
Agung Pratama <agung@sembly.com>
2024-02-25 17:43:32 +0700

    Time: 214 ms (50%), Space: 9.3 MB (100%) - LeetHub

---
+++ 2709-greatest-common-divisor-traversal/2709-greatest-common-divisor-traversal.go
@@ -0,0 +1,128 @@
+func generatePrimes(n int) []int {
+    primes := make([]int, 0)
+    isPrime := make([]bool, n+1)
+    for i:=0;i<=n;i++{
+        isPrime[i] = true
+    }
+    for i:=2;i*i<=n;i++{
+        if !isPrime[i] {
+            continue
+        }
+        
+        for p:=i<<1;p<=n;p+=i{
+            isPrime[p] = false
+        }
+
+    }
+    for i:=2;i<=n;i++{
+        if isPrime[i]{
+            primes = append(primes, i)
+        }
+    }
+    return primes
+}
+var PRIMES []int
+func init() {
+    if len(PRIMES) == 0 {
+        PRIMES = generatePrimes(10000)
+    }
+}
+
+func computePrimeFactor(n int) []int {
+    c := n
+    var factor []int
+    for i:=0;i<len(PRIMES) && PRIMES[i]*PRIMES[i] <= n && c > 1;i++{
+        if c % PRIMES[i] == 0 {
+            factor = append(factor, PRIMES[i])
+            for c > 1 && c % PRIMES[i] == 0 {
+                c /= PRIMES[i]
+            }
+        }
+        
+    }
+    if c > 1 {
+        factor = append(factor, c)
+    }
+    return factor
+}
+
+func canTraverseAllPairs(nums []int) bool {
+    n := len(nums)
+    if n == 1 {
+        return true
+    }
+
+    parents := make([]int, n)
+    for i:=0;i<n;i++{
+        parents[i] = -1
+    }
+
+    primeToLastIdx := make(map[int]int)
+    setLastIdx := func(idx int) {
+        primeFactors := computePrimeFactor(nums[idx])
+        for _, factor := range primeFactors {
+            if _, ok:=primeToLastIdx[factor]; !ok {
+                primeToLastIdx[factor] = idx
+            }else{
+                union(parents, primeToLastIdx[factor], idx)
+                primeToLastIdx[factor] = idx
+            }
+        }
+
+    }
+    for i:=0;i<n;i++{
+        if nums[i] == 1 {
+            return false
+        }
+        setLastIdx(i)
+    }
+
+    for i:=0;i<n;i++{
+        if parents[i] < 0 && -parents[i] == n {
+            return true
+        }
+    }
+    return false
+   
+}
+
+
+func find(parents []int, x int) int {
+	if parents[x] < 0 {
+		return x
+	}
+
+	path := make([]int, 0)
+
+	for parents[x] >= 0 {
+		path = append(path, x)
+		x = parents[x]
+	}
+	for _, p := range path {
+		parents[p] = x
+	}
+	return x
+}
+
+func union(parents []int, a, b int) {
+	if a == b {
+		return
+	}
+	aId := find(parents, a)
+	bId := find(parents, b)
+
+	if aId == bId {
+		return
+	}
+
+	aSize := -parents[aId]
+	bSize := -parents[bId]
+
+	if aSize > bSize {
+		parents[bId] = aId
+		parents[aId] = -aSize - bSize
+	} else {
+		parents[aId] = bId
+		parents[bId] = -aSize - bSize
+	}
+}
