6037c39c5e10125be9de9733faf334a809a1d6f9
Agung Pratama <agung@sembly.com>
2024-02-29 23:16:34 +0700

    Time: 242 ms (92.31%), Space: 15 MB (76.92%) - LeetHub

--- 2940-find-building-where-alice-and-bob-can-meet/2940-find-building-where-alice-and-bob-can-meet.go
+++ 2940-find-building-where-alice-and-bob-can-meet/2940-find-building-where-alice-and-bob-can-meet.go
@@ -1,62 +1,93 @@
+type query struct {
+    a, b, idx int
+}
 func leftmostBuildingQueries(h []int, queries [][]int) []int {
-    nge := computeNGE(h)
-    // fmt.Println("nge", nge)
-    var ans []int
-    for _, query := range queries {
-        a, b := query[0], query[1]
-        if a == b {
-            ans = append(ans, a)
-            continue
-        }else if a > b {
-            a, b = b, a
+    ans := make([]int, len(queries))
+    fq := make([]query, 0)
+    for i:=0;i<len(queries);i++{
+        ans[i] = -1
+        if queries[i][0] > queries[i][1]{
+            queries[i][0], queries[i][1] = queries[i][1], queries[i][0]
         }
-        
-       
-        // now a < b
-        subAns := -1
-        if h[a] < h[b] {
-            subAns = b
-        }else{
-            for b != -1 && h[b] <= h[a]{
-                b = nge[b]
-            }
-            if b != -1 && h[b] > h[a] {
-                subAns = b
-            }
+        a, b := queries[i][0], queries[i][1]
+        if a == b || h[a] < h[b] {
+            ans[i] = b
         }
-        
-        ans = append(ans, subAns)
-
+        if ans[i] == -1 {
+            fq = append(fq, query{a, b, i})
+        }
+    }
+    if len(fq) == 0 {
+        return ans
     }
 
+    sort.Slice(fq, func(a, b int) bool {
+        return fq[a].b > fq[b].b
+    })
+    // fmt.Println("before", ans)
+    // fmt.Println("fq", fq)
+    
+    stack := make([]int, 0)
+    // printStack := func(idxs []int) {
+    //     data := make([]int, 0)
+    //     for _, idx := range idxs{
+    //         data = append(data, h[idx])
+    //     }
+    //     fmt.Println(data)
+    // }
+
+    last := len(h)-1
+    for _, q := range fq {
+        a, b, ansIdx := q.a, q.b, q.idx
+        
+        // we want to insert from n-1 to b+1 in monotonic stack in decreasing manner
+        for i:=last;i>=b+1;i--{
+            for len(stack) > 0 && h[stack[len(stack)-1]] <= h[i] {
+                stack = stack[:len(stack)-1]
+            }
+            stack = append(stack, i)
+        }
+        // printStack(stack)
+        last = b
+        // stack is now in decreasing manner, 6,5,4,2
+        // we want to look the inside the stack the first item that is <= h[a] let say i, then the answer is at i-1
+        idx := sort.Search(len(stack), func(idx int) bool {
+            return h[stack[idx]] <= h[a]
+        })
+        if idx-1 >= 0 {
+            ans[ansIdx] = stack[idx-1]
+        }
+    }
+    
+    
     return ans
 }
 
-func computeNGE(heights []int)[]int {
-    n := len(heights)
-    stack := make([]node, 0)
-    nge := make([]int, n)
+// func computeNGE(heights []int)[]int {
+//     n := len(heights)
+//     stack := make([]node, 0)
+//     nge := make([]int, n)
 
 
-    for i:=0;i<n;i++ {
-        for len(stack) > 0 && stack[len(stack)-1].val < heights[i] {
-            item := stack[len(stack)-1]
-            nge[item.idx] = i
-            stack = stack[:len(stack)-1]
-        }
-        stack = append(stack, node{
-            heights[i],
-            i,
-        })
-    }
-    for len(stack) > 0 {
-        item := stack[len(stack)-1]
-        nge[item.idx] = -1
-        stack = stack[:len(stack)-1]
-    }
-    return nge
-}
+//     for i:=0;i<n;i++ {
+//         for len(stack) > 0 && stack[len(stack)-1].val < heights[i] {
+//             item := stack[len(stack)-1]
+//             nge[item.idx] = i
+//             stack = stack[:len(stack)-1]
+//         }
+//         stack = append(stack, node{
+//             heights[i],
+//             i,
+//         })
+//     }
+//     for len(stack) > 0 {
+//         item := stack[len(stack)-1]
+//         nge[item.idx] = -1
+//         stack = stack[:len(stack)-1]
+//     }
+//     return nge
+// }
 
-type node struct{
-    val, idx int
-}
+// type node struct{
+//     val, idx int
+// }
