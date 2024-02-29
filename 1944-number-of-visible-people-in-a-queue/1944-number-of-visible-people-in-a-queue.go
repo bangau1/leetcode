ce4bf55f6b45b404d3c57e551e950ea05e9f2ead
Agung Pratama <agung@sembly.com>
2024-02-29 23:48:22 +0700

    Time: 154 ms (25%), Space: 8.8 MB (78.26%) - LeetHub

---
+++ 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.go
@@ -0,0 +1,35 @@
+func canSeePersonsCount(h []int) []int {
+    // the general idea is to maintain monotonous stack in decreasing manner from right iteratively
+    // - let say we have maintain monotonous stack until index i
+    // - then people at i-1, they can only see in the stack until the people in stack is greater than it (no equal since unique constraint)
+    stack := make([]int, 0)
+    n := len(h)
+    res := make([]int, n)
+    
+    last := n-1
+    for i:=n-2;i>=0;i--{
+        for ii:=last;ii>=i+1;ii--{
+            for len(stack) > 0 && h[ii] > h[stack[len(stack)-1]] {
+                stack = stack[:len(stack)-1]
+            }
+            stack = append(stack, ii)
+        }
+
+        last = i
+        // look at the smallest idx, such that h[stack[idx]] > h[i]
+        // then the number of people is idx+1
+        // but our stack is reverse actually
+        //  [8,5,1] 3 => 2 people
+        //  search left idx that is < 
+        idx := sort.Search(len(stack), func(idx int) bool {
+            return h[stack[idx]] < h[i]
+        })
+
+        if idx-1 >= 0 {
+            res[i] = len(stack)-(idx-1)
+        }else{
+            res[i] = len(stack)
+        }
+    }
+    return res
+}
