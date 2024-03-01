e3242577e9b7c96b30a5890771ed6b05efd660b0
Agung Pratama <agung@sembly.com>
2024-03-01 17:22:03 +0700

    Time: 4 ms (67.61%), Space: 3 MB (91.75%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.go
+++ 0035-search-insert-position/0035-search-insert-position.go
@@ -1,5 +1,30 @@
 func searchInsert(nums []int, target int) int {
-    return sort.Search(len(nums), func(idx int) bool {
-        return nums[idx] >= target
-    })
+    
+    l, r := 0, len(nums)-1
+    
+    if target < nums[l]{
+        return l
+    }
+    if target > nums[r] {
+        return r+1
+    }
+    var m int
+    for l <= r {
+        m = (r+l)/2
+        if target == nums[m] {
+            return m
+        }else if target > nums[m] {
+            l = m +1
+        }else{
+            r = m-1
+        }
+    }
+    // fmt.Println(m)
+    if nums[m] < target {
+        return m + 1
+    }else if nums[m-1] < target {
+        return m
+    }
+    return m
+    
 }
