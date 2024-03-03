193e5110381fd67c5873993fc4cbb96fd5ca4c33
Agung Pratama <agung@sembly.com>
2024-03-03 10:14:33 +0700

    Time: 2 ms (65.95%), Space: 2.2 MB (19.52%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.go
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.go
@@ -6,20 +6,20 @@
  * }
  */
 func removeNthFromEnd(head *ListNode, n int) *ListNode {
-    // to do it in single pass
-    // we iterate the list one by one
-    // assume we already at 3th element at c = 2 (start at 0), we want to find the 3th last
-    // then it's (c+1-th) element -> 2+1-3 = 0.
-    //
-    //
+    // to do it in single pass:
+    // - we iterate the list one by one
+    // - assume we already at 3th element at c = 2 (start at 0), we want to find the 3th last
+    // - then it's (c+1-th) element -> 2+1-3 = 0.
+    // - then when we keep move forward, c increasing, hence the nth should also be increased
     curr := head
     var prev, nth *ListNode
     c := 0
     
     for curr != nil {
+        // this is when we found the nth element the first time after c iteration
         if c + 1 - n == 0 {
             nth = head
-        }else if c + 1 - n > 0 {
+        }else if c + 1 - n > 0 { // keep moving the nth and prev pointer
             nth, prev = nth.Next, nth
         }
         curr = curr.Next
