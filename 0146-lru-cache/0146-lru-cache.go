5642fc4158fec8a9e0d484478371629ab136618f
Agung Pratama <agung@sembly.com>
2024-03-06 16:47:01 +0700

    Time: 273 ms (98.33%), Space: 224.1 MB (5.1%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.go
+++ 0146-lru-cache/0146-lru-cache.go
@@ -1,115 +1,59 @@
-type Element[K any, V any] struct{
-    Prev *Element[K,V]
-    Next *Element[K,V]
-    Key K
-    Val V
-}
-
-func NewElement[K any, V any](k K, v V) Element[K, V] {
-    return Element[K,V]{
-        Key: k,
-        Val: v,
-    }
-}
-
-func (el *Element[K,V]) addAfter(other *Element[K,V]) {
-    other.Next.Prev = el
-    el.Prev, el.Next = other, other.Next
-    other.Next = el
-}
-
-func (el *Element[K,V]) addBefore(other *Element[K,V]) {
-    other.Prev.Next = el
-    el.Prev, el.Next = other.Prev, other
-    other.Prev = el
-    
-}
+import "container/list"
 
 type LRUCache struct {
-    head, tail *Element[int, int]
-    dict map[int]*Element[int,int]
+    dict map[int]*list.Element
+    queue *list.List
     cap int
+}
+
+type Pair struct{
+    k, v int
 }
 
 
 func Constructor(capacity int) LRUCache {
-    h := NewElement[int, int](-1, -1)
-    t := NewElement[int, int](-1, -1)
-    h.Next, t.Prev = &t, &h
     return LRUCache{
-        head: &h,
-        tail: &t,
+        dict: make(map[int]*list.Element),
+        queue: list.New(),
         cap: capacity,
-        dict: make(map[int]*Element[int,int]),
     }
 }
 
 
 func (this *LRUCache) Get(key int) int {
-    // get from dict[key] -> got the element
-    // - if no, return nil
-    // - if yes, remove element, and put at the head
     el, ok := this.dict[key]
+
     if !ok {
         return -1
     }
 
-    // delete element
-    this.delete(el)
-    // addElement to head
-    el.addAfter(this.head)
-    this.dict[key] = el
-
-    return el.Val
+    // move el to the front
+    this.queue.MoveToFront(el)
+    return el.Value.(Pair).v
 }
 
 
 func (this *LRUCache) Put(key int, value int)  {
-    // defer func(){
-    //     fmt.Println("put", key, value)
-    //     this.Print()
-    // }()
-    // if key exist, update the element
-    // if key isn't exists:
-    // a. If < capacity, then add to the tail
-    // b. if >= capacity, get the tail, update the relevant index
-    if el, ok := this.dict[key]; ok {
-        el.Val = value
-        this.delete(el)
-        this.dict[key] = el
-        el.addAfter(this.head)
+    el, ok := this.dict[key]
+
+    if ok {
+        this.queue.MoveToFront(el)
+        el.Value = Pair{key, value}
         return
     }
+
     size := len(this.dict)
-    el := NewElement[int, int](key, value)
     if size + 1 > this.cap {
-        // get the tail element
-        throwEl := this.tail.Prev
-        // remove the tail.key from the dict
-        this.delete(throwEl)
+        // remove the back
+        back := this.queue.Back()
+        this.queue.Remove(back)
+        delete(this.dict, back.Value.(Pair).k)
     }
-    // add after head
-    this.dict[key] = &el
-    el.addAfter(this.head)
+    el = this.queue.PushFront(Pair{key, value})
+    this.dict[key] = el
 }
 
-func (this *LRUCache) Print() {
-    curr := this.head.Next
-    var res [][2]int
-    for curr != this.tail{
-        res = append(res, [2]int{curr.Key, curr.Val})
-        curr = curr.Next
-    }
-    fmt.Println(res)
-}
-
-func (this *LRUCache) delete(el *Element[int, int]) {
-    delete(this.dict, el.Key)
-    el.Next.Prev = el.Prev
-    el.Prev.Next = el.Next
-} 
-
-
+func init() { debug.SetGCPercent(-1) }
 /**
  * Your LRUCache object will be instantiated and called as such:
  * obj := Constructor(capacity);
