70b8f247b23bc4c054aeda910808fe47682fff03
Agung Pratama <agung@sembly.com>
2024-03-09 10:03:19 +0700

    Time: 68 ms (69.51%), Space: 8.4 MB (100%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.go
+++ 2540-minimum-common-value/2540-minimum-common-value.go
@@ -3,9 +3,9 @@ func getCommon(nums1 []int, nums2 []int) int {
         nums1, nums2 = nums2, nums1
     }
     ln, rn := len(nums1), len(nums2)
-
+    i := sort.SearchInts(nums1, nums2[0])
     var idx int
-    for i:=0;i<ln;i++{
+    for ;i<ln;i++{
         idx = sort.SearchInts(nums2, nums1[i])
         if idx < rn && nums2[idx] == nums1[i] {
             return nums1[i]
