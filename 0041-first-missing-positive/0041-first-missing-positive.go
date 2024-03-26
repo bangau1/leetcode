30bc1e0ab80dad369c7132ea3dd263ca7dd48ae8
Agung Pratama <agung@sembly.com>
2024-03-26 10:01:11 +0700

    Time: 54 ms (42.14%), Space: 8.5 MB (35.89%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.go
+++ 0041-first-missing-positive/0041-first-missing-positive.go
@@ -1,27 +1,25 @@
 func firstMissingPositive(nums []int) int {
-    // we can't use min/max, since the missing can be in the middle of min-max
-    // observations:
-    // n = length(nums)
-    // the smallest missing positive number can be anywhere of 1-n (inclusive)
-    // in order to avoid additional space, we can modify the existing nums array
-    // if we find a[i] that is positive, we set a[a[i]] = a[i]
-    //
-    // basically we want to convert the nums as hash map to indicate that the number has been marked
-    // the unmarked one is basically the missing positive integer
-
-
-    for i:=1;i<=len(nums);i++{
-        tmp := nums[i-1]
-        for tmp <= len(nums) && tmp >= 1 && tmp != nums[tmp-1]{
-            tmp, nums[tmp-1] = nums[tmp-1], tmp
-        }
-
-    }
-    // fmt.Println(nums)
-    for i:=1;i<=len(nums);i++{
-        if nums[i-1] != i {
-            return i
+/*    
+observations:
+- we only care about the positive integers, 
+- perhaps we can edit in place in the nums. if found A = nums[i], the set nums[A-1]=A
+*/
+    n := len(nums)
+    var a, temp int
+    for i:=0;i<n;i++{
+        if nums[i] > 0 {
+            a = nums[i]
+            for a > 0 && a-1 < n && nums[a-1]!=a {
+                temp = nums[a-1]
+                nums[a-1] = a
+                a = temp
+            }
         }
     }
-    return len(nums)+1
+    for i:=0;i<n;i++{
+        if nums[i] != i+1 {
+            return i+1
+        }
+    }
+    return n+1
 }
