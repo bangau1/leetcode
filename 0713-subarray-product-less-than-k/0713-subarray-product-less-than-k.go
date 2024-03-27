83a5441ba684cdf967c2c1d3e4d6857c1daf1132
Agung Pratama <agung@sembly.com>
2024-03-27 21:16:03 +0700

    Time: 84 ms (18.4%), Space: 8 MB (9.6%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.go
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.go
@@ -1,117 +1,55 @@
-func gcd(a, b int) int {
-    if b == 0 {
-        return a
-    }
-    if a == 0 {
-        return b
-    }
+func getGCD(a, b int) int {
     if a < b {
         a, b = b, a
     }
-    return gcd(b, a % b)
+
+    if b == 0 {
+        return a
+    }
+
+    return getGCD(b, a % b)
 }
 
-func originalSolution(nums []int, k int) int {
-    if k == 0 {
-        return 0
-    }
-
-    n:=len(nums)
-
-    sol := make([][]int, 0)
-    left, right:=0,0
-    dot := 1
-    prevDot := 1
-    
-    for left < n && right < n {
-        prevDot = dot
-        dot *= nums[right]
-        // fmt.Println("dot", dot, "from", nums[left:right+1])
-
-        if dot < k {
-            right++
-        }else{
-            if prevDot < k {
-                sol = append(sol, []int{left, right-1})
-            }
-            for dot >= k && left <= right{
-                dot /= nums[left]
-                left++
-            }
-            right++
-        }
-    }
-    if prevDot < k {
-        sol = append(sol, []int{left, right-1})
-    }
-    // fmt.Println(sol)
-    var total int
-    for i:=0;i<len(sol);i++{
-        left, right := sol[i][0], sol[i][1]
-        arrLen := (right-left+1)
-        combi := 0
-        if arrLen % 2 == 0{
-            combi = arrLen/2 * (arrLen+1)
-        }else{
-            combi = (arrLen+1)/2 * arrLen
-        }
-
-        if i > 0 {
-            // check overlap range
-            _, prevRight := sol[i-1][0], sol[i-1][1]
-            // overlap
-            if prevRight >= left {
-                overlap := prevRight-left+1
-                combi -= (overlap*(overlap+1))/2 
-            }
-        }
-        // fmt.Println(sol[i], combi)
-        total+=combi
-    }
-    return total
-}
 
 func numSubarrayProductLessThanK(nums []int, k int) int {
-    return simpleSolution(nums, k)
-}
+    // since len(nums) is quite big, the product of all elements in the subarray can be really large, doesn't fit in the int64
+    // so one thing that we can optimize is to divide the product of all element by the gcd(k, product(arr))
+    // assume there is subarray of [a, b, c], to check whether a*b*c < k, we can divide both by the gcd in iterative manner:
+    // - a/gcd(a,k) * b/gcd(b, k') * c /gcd(c, k'') < k''
 
-func simpleSolution(nums []int, k int) int {
-    if k <= 1 {
-        return 0
-    }
+    // now how to compute how many subarray on that? we can use sliding window that will expand as long as the condition true.
+    // if not satisfied, we decrease the window 
+    // everytime we increase the window length, then that increase the total subset += len(window)
 
-    n:=len(nums)
-    l := 0
-    dot := 1
-    total := 0
-    for r:=0;r<n;r++{
-        dot *= nums[r]
+    r := 0
 
-        for dot >= k {
-            dot /= nums[l]
-            l++
+    var gcd int
+    var prod = 1
+    var sum = 0
+
+    window := make([][2]int, 0)
+    for r < len(nums) {
+        gcd = getGCD(nums[r], k)
+        // gcd = 1
+        prod = prod * nums[r]/gcd
+        k = k /gcd
+
+        window = append(window, [2]int{nums[r], gcd})
+        
+        if prod < k {
+            sum += len(window)
+        }else{
+            for len(window) > 0 && prod >= k {
+
+                prod = prod * window[0][1]/ window[0][0]
+                k = k * window[0][1]
+                window = window[1:]
+            }
+
+            sum += len(window)
         }
-        total += r-l +1
+        r++
     }
 
-    return total
+    return sum
 }
-
-
-// a, b, c, d
-// if a..c and b...d is the max subarray we found, then 
-// the number of subarray are:
-// a,b,c -> there are n * n+1/2 = 6
-// b,c,d -> there are 6
-// overlap b,c ->3 combination 
-// total 6+6-3=9
-
-
-// 10,5 = 3 combi
-// 5,2,6 = 6 combi
-// 5 = 1 combi
-// 9-1=8
-
-// 1 1 1
-//  2. 2
-//.   3
