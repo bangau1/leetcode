79106c1c66ec3a7dedcfecd9f1245e9805786f13
Agung Pratama <agung@sembly.com>
2024-03-27 21:44:44 +0700

    Time: 173 ms (18.18%), Space: 32.2 MB (6.06%) - LeetHub

---
+++ 2302-count-subarrays-with-score-less-than-k/2302-count-subarrays-with-score-less-than-k.go
@@ -0,0 +1,27 @@
+func countSubarrays(nums []int, k int64) int64 {
+    window := make([]data, 0)
+    
+    var sum int64
+    var count int64
+    for r:=0;r<len(nums);r++{
+        sum += int64(nums[r])
+        window = append(window, data{r, sum})
+
+        if sum * int64(len(window)) < k {
+            count += int64(len(window))
+        }else{
+            for len(window) > 0 && sum * int64(len(window)) >= k {
+                sum -= int64(nums[window[0].index])
+                window = window[1:]
+            }
+            count += int64(len(window))
+        }
+    }
+    return count
+
+}
+
+type data struct {
+    index int
+    sum int64
+}
