c310f935fe0b4c9beccf86d7c55a3b05f25d8986
Agung Pratama <agung@sembly.com>
2024-03-30 19:08:14 +0700

    Time: 2 ms (80.95%), Space: 2.5 MB (14.29%) - LeetHub

---
+++ 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.go
@@ -0,0 +1,33 @@
+
+func countVowelSubstrings(word string) int {
+    fmt.Println(countAtMostKVowels(word, 5))
+    fmt.Println(countAtMostKVowels(word, 4))
+    return countAtMostKVowels(word, 5) - countAtMostKVowels(word, 4)
+}
+
+func countAtMostKVowels(word string, k int) int {
+    counter := make(map[byte]int)
+    var total int
+    var l int
+    for r := 0;r<len(word);r++{
+        if !isVowel(word[r]) {
+            l = r + 1
+            counter = make(map[byte]int)
+            continue
+        }
+        counter[word[r]]++
+        for l<=r && len(counter) > k {
+            counter[word[l]]--
+            if counter[word[l]] == 0 {
+                delete(counter, word[l])
+            }
+            l++
+        }
+        total += r-l+1
+    }
+    return total
+}
+
+func isVowel(char byte) bool {
+    return char == 'a' || char == 'i' || char == 'u' || char == 'e' || char == 'o'
+}
