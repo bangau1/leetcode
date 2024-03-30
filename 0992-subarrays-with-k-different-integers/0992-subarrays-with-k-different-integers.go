c7b706aa309a0611af5ad65f894a346d503f5f49
Agung Pratama <agung@sembly.com>
2024-03-31 01:41:02 +0700

    Time: 53 ms (90.63%), Space: 29.4 MB (6.25%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.go
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.go
@@ -3,34 +3,49 @@ import (
 )
 
 var counter = make([]int, 20000+1)
+
+/*
+general idea:
+- need to maintain a counter map (or use array as hashmap and additional countDist to track the distinct count)
+- if countDist < k, keep increasing the window (increasing the right)
+- if countDist > k, reduce the window, by increasnig the left
+- if countDist == k, then we can start to increment the total good subarray. Observation:
+  a. [left...right] is the maximum subarray that start with nums[left] and ends with nums[right] element. 
+  b. how many good subarray is there from left...right subarray, that ends with nums[right] position?
+      - Let the mostLeftIdx = min(lastIdx(A1), lastIdx(A2), ..., lastIdx(Ak)), where A1 to Ak denotes the k distinct numbers in the subarray.
+      - then total subarray is = mostLeftIdx - left + 1.
+      - For example, given subarray: [1,2,1,2] and k = 2.
+        - left = 0, right = 3
+        - mostLeftIdx = min(lastIdx(1), lastIdx(2)) = min(2, 3) = 2
+        - then total subarray is = 2 - 0 + 1 = 3. That is {[1,2,1,2], [2,1,2], [1,2]}
+
+    Question is: how to compute (and update) mostLeftIdx efficiently in O(1)?
+    - We can use 2 data structure to help on this: 
+      - double linked list to track the last index of distinct number
+      - hashmap to track the pointer of such distinct number inside the double linked list, so we can easily delete that element in O(1), without iterating the double linked list 
+
+*/
 func subarraysWithKDistinct(nums []int, k int) int {
-    // general idea:
-    // - need to maintain a counter map
-    // - if len(counter) < k, keep increasing the window
-    // - if len(counter) > k, reduce the window, by increase the left part of the window
-    // - if len(counter) == k, then 
-    //   - left to right is the maximum length of such subarray on that window. To know how many subarray in total for a given window, do it in naive way?
-    //   - try to increase the left (in temp variable) until the len(counter) < k
-    //
-    // further improve on how to avoid naive way when len(counter) == k
-    // - we keep the lastIndex on certain data structure
-    // - then get the first minimum value, then the subtotal+= minValue - left + 1
-    arrFills(counter, 0)
-    countDist := 0
-    lastIdxWindow := NewLastIdxWindow()
+
+    arrFills(counter, 0) // to track the number occurences
+    countDist := 0 // to track how many distinct number
+    lastIdxWindow := NewLastIdxWindow() // to track the last index of certain number
 
     var left, total int
     n := len(nums)
     
-    for r := 0;r < n;r++{
-        if counter[nums[r]] == 0 {
-            countDist++
-        }
-        counter[nums[r]]++
-        lastIdxWindow.Append(nums[r], r)
+    for right := 0;right < n;right++{
 
-        for left <= r && countDist > k {
+        counter[nums[right]]++
+        if counter[nums[right]] == 1 {
+            countDist++ // increment the countDistinct
+        }
+        lastIdxWindow.UpdateLastIdx(nums[right], right)
+
+        // reduce the window by increasing the left pointer
+        for left <= right && countDist > k {
             counter[nums[left]]--
+            // if the nums[left] occurences is 0, then we need to update the countDist and remove the lastIdx of such element
             if counter[nums[left]] == 0 {
                 countDist--
                 lastIdxWindow.RemoveNum(nums[left])
@@ -39,7 +54,7 @@ func subarraysWithKDistinct(nums []int, k int) int {
         }
 
         if countDist == k {
-            total += lastIdxWindow.GetLeft() - left + 1
+            total += lastIdxWindow.GetMostLeftIndex() - left + 1
         }
 
     }
@@ -65,7 +80,7 @@ func NewLastIdxWindow() *LastIdxWindow {
     }
 }
 
-func (w *LastIdxWindow) Append(num int, idx int) {
+func (w *LastIdxWindow) UpdateLastIdx(num int, idx int) {
     element, found := w.lastIdxMap[num]
 
     if found {
@@ -75,7 +90,7 @@ func (w *LastIdxWindow) Append(num int, idx int) {
     w.lastIdxMap[num] = newElement
 }
 
-func (w *LastIdxWindow) GetLeft() int {
+func (w *LastIdxWindow) GetMostLeftIndex() int {
     return w.window.Front().Value.(int)
 }
 
