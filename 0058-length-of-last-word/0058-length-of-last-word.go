b64da5ab2b3f5efd8ea3695f22dddcbae6cb2b8b
Agung Pratama <agung@sembly.com>
2024-04-01 14:59:02 +0700

    Time: 0 ms (100%), Space: 2.4 MB (28.26%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.go
+++ 0058-length-of-last-word/0058-length-of-last-word.go
@@ -2,15 +2,12 @@ func lengthOfLastWord(s string) int {
     n := len(s)
     length := 0
     i := n-1
-    for i >= 0 {
-        for i >= 0 && s[i] == ' '{
-            i--
-        }
-        for i >= 0 && s[i] != ' '{
-            i--
-            length++
-        }
-        return length
+    for i >= 0 && s[i] == ' '{
+        i--
+    }
+    for i >= 0 && s[i] != ' '{
+        i--
+        length++
     }
     return length
 }
