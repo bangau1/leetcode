8a659aae9ca74fb34e5a20ff0699ebac34535c61
Agung Pratama <agung@sembly.com>
2024-04-04 09:32:10 +0700

    Time: 1 ms (69.57%), Space: 2.2 MB (28.26%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.go
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.go
@@ -1,17 +1,17 @@
 var open, close byte = '(', ')'
 
 func maxDepth(s string) int {
-    stack := make([]byte, 0)
+    var stack int
     maxDepth := 0
 
     for i:=0;i<len(s);i++{
         if s[i] == open {
-            stack = append(stack, s[i])
+            stack++
         }else if s[i] == close {
-            if maxDepth < len(stack) {
-                maxDepth = len(stack)
+            if maxDepth < stack {
+                maxDepth = stack
             }
-            stack = stack[:len(stack)-1]
+            stack--
         }
     }
 
