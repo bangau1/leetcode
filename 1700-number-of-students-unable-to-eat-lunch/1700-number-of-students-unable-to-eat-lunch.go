5b45df766d22d4cdb8337d70389a29b2853298b3
Agung Pratama <agung@sembly.com>
2024-04-08 12:26:15 +0700

    Time: 2 ms (43.04%), Space: 2.4 MB (40.51%) - LeetHub

---
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.go
@@ -0,0 +1,28 @@
+func countStudents(students []int, sandwiches []int) int {
+    n := len(students)
+    pref := [2]int{0,0}
+
+    for i:=0;i<n;i++{
+        pref[students[i]]++
+    }
+
+    for len(students) > 0 {
+        for len(students) > 0 && students[0] == sandwiches[0] {
+            pref[students[0]]--
+            students = students[1:]
+            sandwiches = sandwiches[1:]
+        }
+
+        for len(students) > 0 && students[0] != sandwiches[0] && pref[sandwiches[0]] > 0 {
+            topStudent := students[0]
+            students = students[1:]
+            students = append(students, topStudent)
+        }
+
+        if len(sandwiches) > 0 && pref[sandwiches[0]] == 0 {
+            break
+        }
+    }
+
+    return len(students)
+}
