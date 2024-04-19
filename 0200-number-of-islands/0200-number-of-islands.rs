f168991af8daf173cd002e9ecb6587e96fdaa660
Agung Pratama <agung@sembly.com>
2024-04-19 13:41:16 +0700

    Time: 15 ms (13.77%), Space: 9 MB (32.46%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.rs
+++ 0200-number-of-islands/0200-number-of-islands.rs
@@ -1,8 +1,10 @@
+
 impl Solution {
     pub fn num_islands(grid: Vec<Vec<char>>) -> i32 {
         let size = grid.len() * grid[0].len();
         let mut ds = DisjointSet::new(size as u32);
         let col_size = grid[0].len();
+        let mut last_zero_id: Option<u32> = None;
         for r in 0..grid.len() {
             for c in 0..grid[r].len() {
                 if grid[r][c] == '1' {
@@ -13,26 +15,21 @@ impl Solution {
                     if c >= 1 && grid[r][c - 1] == '1' {
                         ds.union(curr_cell_id, to_cell_id(r, c - 1, col_size));
                     }
-                }else {
-                    let curr_cell_id = to_cell_id(r, c, grid[0].len());
-                    if r >= 1 && grid[r - 1][c] != '1' {
-                        ds.union(curr_cell_id, to_cell_id(r - 1, c, col_size));
+                } else {
+                    let curr_cell_id = to_cell_id(r, c, col_size);
+
+                    if last_zero_id != None {
+                        ds.union(curr_cell_id, last_zero_id.unwrap());
                     }
-                    if c >= 1 && grid[r][c - 1] != '1' {
-                        ds.union(curr_cell_id, to_cell_id(r, c - 1, col_size));
-                    }
+                    last_zero_id = Some(curr_cell_id);
                 }
             }
         }
-        let mut total = 0;
-        let union_ids = ds.get_all_union_ids();
-        for &id in union_ids.iter() {
-            let (r, c) = cell_id_to_row_col(id as u32, col_size);
-            if grid[r][c] == '1' {
-                total += 1;
-            }
+        let mut total = ds.total_set();
+        if last_zero_id != None {
+            total -= 1;
         }
-        return total;
+        return total as _;
     }
 }
 
@@ -40,13 +37,6 @@ fn to_cell_id(row: usize, col: usize, col_size: usize) -> u32 {
     return (row * col_size + col) as u32;
 }
 
-fn cell_id_to_row_col(cell_id: u32, col_size: usize) -> (usize, usize) {
-    let col_size = col_size as u32;
-    let row = cell_id / col_size;
-    let col = cell_id % col_size;
-
-    return (row as usize, col as usize);
-}
 
 pub struct DisjointSet {
     parents: Vec<i32>,
