893252004b0405125b1acdfdb35be0d8abd79cf8
Agung Pratama <agung@sembly.com>
2024-04-19 10:19:18 +0700

    Time: 16 ms (12.79%), Space: 9.2 MB (8.2%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.rs
+++ 0200-number-of-islands/0200-number-of-islands.rs
@@ -6,20 +6,20 @@ impl Solution {
         for r in 0..grid.len() {
             for c in 0..grid[r].len() {
                 if grid[r][c] == '1' {
-                    let curr_cell_id = to_cell_id(r, c, &grid);
+                    let curr_cell_id = to_cell_id(r, c, grid[0].len());
                     if r >= 1 && grid[r - 1][c] == '1' {
-                        ds.union(curr_cell_id, to_cell_id(r - 1, c, &grid));
+                        ds.union(curr_cell_id, to_cell_id(r - 1, c, grid[0].len()));
                     }
                     if c >= 1 && grid[r][c - 1] == '1' {
-                        ds.union(curr_cell_id, to_cell_id(r, c - 1, &grid));
+                        ds.union(curr_cell_id, to_cell_id(r, c - 1, grid[0].len()));
                     }
                 }else {
-                    let curr_cell_id = to_cell_id(r, c, &grid);
+                    let curr_cell_id = to_cell_id(r, c, grid[0].len());
                     if r >= 1 && grid[r - 1][c] != '1' {
-                        ds.union(curr_cell_id, to_cell_id(r - 1, c, &grid));
+                        ds.union(curr_cell_id, to_cell_id(r - 1, c, grid[0].len()));
                     }
                     if c >= 1 && grid[r][c - 1] != '1' {
-                        ds.union(curr_cell_id, to_cell_id(r, c - 1, &grid));
+                        ds.union(curr_cell_id, to_cell_id(r, c - 1, grid[0].len()));
                     }
                 }
             }
@@ -27,7 +27,7 @@ impl Solution {
         let mut total = 0;
         let union_ids = ds.get_all_union_ids();
         for &id in union_ids.iter() {
-            let (r, c) = cell_id_to_row_col(id as u32, &grid);
+            let (r, c) = cell_id_to_row_col(id as u32, grid[0].len());
             if grid[r][c] == '1' {
                 total += 1;
             }
@@ -36,14 +36,12 @@ impl Solution {
     }
 }
 
-fn to_cell_id(row: usize, col: usize, grid: &Vec<Vec<char>>) -> u32 {
-    let col_size = grid[0].len();
-
+fn to_cell_id(row: usize, col: usize, col_size: usize) -> u32 {
     return (row * col_size + col) as u32;
 }
 
-fn cell_id_to_row_col(cell_id: u32, grid: &Vec<Vec<char>>) -> (usize, usize) {
-    let col_size = grid[0].len() as u32;
+fn cell_id_to_row_col(cell_id: u32, col_size: usize) -> (usize, usize) {
+    let col_size = col_size as u32;
     let row = cell_id / col_size;
     let col = cell_id % col_size;
 
