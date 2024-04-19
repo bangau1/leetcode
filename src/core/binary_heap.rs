78636c26d39afdc28972169ed7b0a2e59a2cf769
Agung Pratama <agp.csui08@gmail.com>
2024-04-19 14:10:47 +0700

    Allow unused and move binary_heap to test (since it's only for testing the binary_heap in rust)

--- src/core/binary_heap.rs
+++ src/core/binary_heap.rs
@@ -1,159 +1,161 @@
-use std::{
-    cmp::{Ord, PartialOrd, Reverse},
-    collections::BinaryHeap,
-};
+#[cfg(test)]
+mod test {
 
-#[derive(Debug, PartialEq, Eq)]
-struct Vertex {
-    node: u32,
-    weight: i32,
-}
+    use std::cmp::{Ord, PartialOrd, Reverse};
+    use std::collections::BinaryHeap;
 
-impl PartialOrd for Vertex {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        return Some(self.cmp(other));
+    #[derive(Debug, PartialEq, Eq)]
+    struct Vertex {
+        node: u32,
+        weight: i32,
     }
-}
 
-impl Ord for Vertex {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        if self.weight == other.weight {
-            return self.node.cmp(&other.node);
+    impl PartialOrd for Vertex {
+        fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+            return Some(self.cmp(other));
         }
-        return self.weight.cmp(&other.weight);
     }
-}
 
-#[test]
-fn test_max_heap() {
-    let mut max_heap = BinaryHeap::new();
+    impl Ord for Vertex {
+        fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+            if self.weight == other.weight {
+                return self.node.cmp(&other.node);
+            }
+            return self.weight.cmp(&other.weight);
+        }
+    }
 
-    max_heap.push(1);
-    max_heap.push(100);
+    #[test]
+    fn test_max_heap() {
+        let mut max_heap = BinaryHeap::new();
 
-    assert_eq!(100, max_heap.pop().unwrap());
-}
+        max_heap.push(1);
+        max_heap.push(100);
 
-#[test]
-fn test_min_heap_using_reverse() {
-    let mut min_heap = BinaryHeap::new();
+        assert_eq!(100, max_heap.pop().unwrap());
+    }
 
-    min_heap.push(Reverse(1));
-    min_heap.push(Reverse(100));
+    #[test]
+    fn test_min_heap_using_reverse() {
+        let mut min_heap = BinaryHeap::new();
 
-    assert_eq!(1, min_heap.pop().unwrap().0);
-}
+        min_heap.push(Reverse(1));
+        min_heap.push(Reverse(100));
 
-#[test]
-fn test_max_heap_on_struct() {
-    let mut max_heap = BinaryHeap::new();
+        assert_eq!(1, min_heap.pop().unwrap().0);
+    }
 
-    max_heap.push(Vertex {
-        node: 1,
-        weight: 100,
-    });
-    max_heap.push(Vertex {
-        node: 2,
-        weight: 100,
-    });
-    max_heap.push(Vertex {
-        node: 3,
-        weight: 200,
-    });
+    #[test]
+    fn test_max_heap_on_struct() {
+        let mut max_heap = BinaryHeap::new();
 
-    assert_eq!(
-        Vertex {
+        max_heap.push(Vertex {
+            node: 1,
+            weight: 100,
+        });
+        max_heap.push(Vertex {
+            node: 2,
+            weight: 100,
+        });
+        max_heap.push(Vertex {
             node: 3,
-            weight: 200
-        },
-        max_heap.pop().unwrap()
-    );
-    assert_eq!(
-        Vertex {
-            node: 2,
-            weight: 100
-        },
-        max_heap.pop().unwrap()
-    );
-}
+            weight: 200,
+        });
 
-#[test]
-fn test_max_heap_on_struct_with_reverse() {
-    let mut min_heap = BinaryHeap::new();
-
-    min_heap.push(Reverse(Vertex {
-        node: 1,
-        weight: 100,
-    }));
-    min_heap.push(Reverse(Vertex {
-        node: 2,
-        weight: 100,
-    }));
-    min_heap.push(Reverse(Vertex {
-        node: 3,
-        weight: 200,
-    }));
-
-    assert_eq!(
-        Vertex {
-            node: 1,
-            weight: 100
-        },
-        min_heap.pop().unwrap().0
-    );
-    assert_eq!(
-        Vertex {
-            node: 2,
-            weight: 100
-        },
-        min_heap.pop().unwrap().0
-    );
-}
-
-#[derive(Debug, PartialEq, Eq)]
-struct ReverseVertex(Vertex);
-
-impl PartialOrd for ReverseVertex {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        return Some(self.cmp(other));
+        assert_eq!(
+            Vertex {
+                node: 3,
+                weight: 200
+            },
+            max_heap.pop().unwrap()
+        );
+        assert_eq!(
+            Vertex {
+                node: 2,
+                weight: 100
+            },
+            max_heap.pop().unwrap()
+        );
     }
-}
 
-impl Ord for ReverseVertex {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        // we reverse the comparison from self->other to other->self
-        return other.0.cmp(&self.0);
-    }
-}
+    #[test]
+    fn test_max_heap_on_struct_with_reverse() {
+        let mut min_heap = BinaryHeap::new();
 
-#[test]
-fn test_max_heap_on_struct_with_wrapper_struct() {
-    let mut min_heap = BinaryHeap::new();
-    min_heap.push(ReverseVertex(Vertex {
-        node: 1,
-        weight: 100,
-    }));
-    min_heap.push(ReverseVertex(Vertex {
-        node: 2,
-        weight: 100,
-    }));
-    min_heap.push(ReverseVertex(Vertex {
-        node: 3,
-        weight: 200,
-    }));
-
-    assert_eq!(
-        ReverseVertex(Vertex {
+        min_heap.push(Reverse(Vertex {
             node: 1,
-            weight: 100
-        }),
-        min_heap.pop().unwrap()
-    );
-    assert_eq!(
-        ReverseVertex(Vertex {
+            weight: 100,
+        }));
+        min_heap.push(Reverse(Vertex {
             node: 2,
-            weight: 100
-        }),
-        min_heap.pop().unwrap()
-    );
+            weight: 100,
+        }));
+        min_heap.push(Reverse(Vertex {
+            node: 3,
+            weight: 200,
+        }));
+
+        assert_eq!(
+            Vertex {
+                node: 1,
+                weight: 100
+            },
+            min_heap.pop().unwrap().0
+        );
+        assert_eq!(
+            Vertex {
+                node: 2,
+                weight: 100
+            },
+            min_heap.pop().unwrap().0
+        );
+    }
+
+    #[derive(Debug, PartialEq, Eq)]
+    struct ReverseVertex(Vertex);
+
+    impl PartialOrd for ReverseVertex {
+        fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+            return Some(self.cmp(other));
+        }
+    }
+
+    impl Ord for ReverseVertex {
+        fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+            // we reverse the comparison from self->other to other->self
+            return other.0.cmp(&self.0);
+        }
+    }
+
+    #[test]
+    fn test_max_heap_on_struct_with_wrapper_struct() {
+        let mut min_heap = BinaryHeap::new();
+        min_heap.push(ReverseVertex(Vertex {
+            node: 1,
+            weight: 100,
+        }));
+        min_heap.push(ReverseVertex(Vertex {
+            node: 2,
+            weight: 100,
+        }));
+        min_heap.push(ReverseVertex(Vertex {
+            node: 3,
+            weight: 200,
+        }));
+
+        assert_eq!(
+            ReverseVertex(Vertex {
+                node: 1,
+                weight: 100
+            }),
+            min_heap.pop().unwrap()
+        );
+        assert_eq!(
+            ReverseVertex(Vertex {
+                node: 2,
+                weight: 100
+            }),
+            min_heap.pop().unwrap()
+        );
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#![allow(unused)]
 pub mod core;
 pub mod graph;
 mod solution;
