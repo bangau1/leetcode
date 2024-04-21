140a3016ab06cb58a0be3aa670f42966de758c67
Agung Pratama <agung@sembly.com>
2024-04-22 05:31:35 +0700

    Time: 221 ms (91.7%), Space: 32.3 MB (87.55%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.go
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.go
@@ -1,95 +1,31 @@
-type UnionSet struct {
-    arr []int
-    // maxSize int
-}
-
-type Edge struct {
-    from int
-    to int
-}
-
-func NewUnionSet(nodeSize int, edges []Edge) *UnionSet {
-    res := UnionSet{}
-    res.arr = make([]int, nodeSize)
-    for i := 0; i<nodeSize;i++{
-        res.arr[i] = -1 // each is group set with 1 size (negative value)
-    }
-
-    for _, edge := range edges {
-        res.Union(edge.from, edge.to)
-    }
-
-    return &res
-}
-func (u *UnionSet) FindGroup(i int) int {
-    // fmt.Println("FindGroup", i)
-    // fmt.Println("Arr", u.arr)
-    // check if i is the root of the group 
-    if u.arr[i] < 0 {
-        return i
-    }
-    path := make([]int, 0)
-    
-    // while it's not a group
-    for u.arr[i] >= 0 {
-        fmt.Println(i, u.arr[i])
-        path = append(path, i)
-        i = u.arr[i]
-    }
-    // now i should be the root
-    
-    // path compression optimization
-    for _, p := range path {
-        u.arr[p] = i
-    }
-
-    return i
-}
-
-func (u *UnionSet) Union(f, p int) {
-    // fmt.Println("??Union", f, p, u.arr)
-    fGroup := u.FindGroup(f)
-    pGroup := u.FindGroup(p)
-    if fGroup == pGroup {
-        return
-    }
-
-    fGroupSize := u.arr[fGroup] * -1
-    pGroupSize := u.arr[pGroup] * -1
-
-    
-
-    // fmt.Println("fGroup", fGroup, "pGroup", pGroup)
-    // fmt.Println("fGroupSize", fGroupSize, "pGroupSize", pGroupSize)
-    if fGroupSize <= 0 || pGroupSize <= 0 {
-        panic("unexpected logic")
-    }
-    // merge f to p set
-    if fGroupSize < pGroupSize {
-        u.arr[pGroup] = -fGroupSize - pGroupSize // store as negative value
-        u.arr[fGroup] = pGroup
-        // fmt.Println("Merge f to p", u.arr)
-    } else { // merge p to f set   
-        u.arr[fGroup] = -fGroupSize - pGroupSize // store as negative value
-        u.arr[pGroup] = fGroup
-        // fmt.Println("Merge p to f", u.arr)
-    }
-}
-
 func validPath(n int, edges [][]int, source int, destination int) bool {
-    edgesArr := make([]Edge, 0)
+    visited := make([]bool, n)
+    adjList := make([][]int, n)
     for _, edge := range edges {
-        edgeStruct := Edge {
-            from: edge[0],
-            to: edge[1],
-        }
-        edgesArr = append(edgesArr, edgeStruct)
+        adjList[edge[0]] = append(adjList[edge[0]], edge[1])
+        adjList[edge[1]] = append(adjList[edge[1]], edge[0])
     }
 
-    unionSet := NewUnionSet(n, edgesArr)
-    // fmt.Println("===========", unionSet.arr)
-    sourceGroup := unionSet.FindGroup(source)
-    destGroup := unionSet.FindGroup(destination)
+    q := make([]int, 0)
+    q = append(q, source)
+    var node int
+    for len(q) > 0 {
+        node = q[0]
+        q = q[1:]
 
-    return sourceGroup == destGroup
+        if node == destination {
+            return true
+        } 
+        if visited[node] {
+            continue
+        }
+        visited[node] = true
+
+        for _, next := range adjList[node] {
+            if !visited[next] {
+                q = append(q, next)
+            }
+        }
+    }
+    return false
 }
