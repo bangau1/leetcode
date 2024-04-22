91888ad69cbd3c95e56bd9209d7ffb685be67228
Agung Pratama <agung@sembly.com>
2024-04-22 15:05:24 +0700

    Time: 445 ms (5.49%), Space: 9.2 MB (7.69%) - LeetHub

--- 0752-open-the-lock/0752-open-the-lock.go
+++ 0752-open-the-lock/0752-open-the-lock.go
@@ -1,83 +1,178 @@
+
 type Wheels struct {
-    data [4]byte
+	data [4]byte
 }
 
 func (w *Wheels) String() string {
-    res := make([]byte, 0)
-    res[0] = w.data[0]
-    
+	if w == nil {
+		return ""
+	}
+	res := make([]byte, 4)
+	res[0] = w.data[0]
+	res[1] = w.data[1]
+	res[2] = w.data[2]
+	res[3] = w.data[3]
+	return string(res)
 }
 
 type Vertex struct {
-    w Wheels
-    c int
+	w Wheels
+	c int
+}
+
+func (v *Vertex) getEstCost(target Wheels) int {
+	cost := 0
+	var minCost = 0
+	// 0 -> 7 can be: 0->1->2->...->7 = 7 steps, or 0->9->8->7 = 3 steps
+	// 9->7
+	for i := 0; i < 4; i++ {
+		left, right := v.w.data[i], target.data[i]
+		if left == right {
+			continue
+		}
+
+		if left > right {
+			left, right = right, left
+		}
+
+		minCost = min(
+			int(right-left),
+			10-int(right+left),
+		)
+
+		cost += minCost
+	}
+
+	return cost
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
 }
 
 func NewWheels(str string) Wheels {
-    return Wheels {
-        data: [4]byte{str[0], str[1], str[2], str[3]},
-    }
+	return Wheels{
+		data: [4]byte{str[0], str[1], str[2], str[3]},
+	}
+}
+
+func NewWheelsFromWheels(other Wheels) Wheels {
+	return Wheels{
+		data: [4]byte{other.data[0], other.data[1], other.data[2], other.data[3]},
+	}
 }
 
 func (w *Wheels) getNextSteps() []Wheels {
-    
-    res := make([]Wheels, 0)
-    for i:=0;i<4;i++{
-        currDigit := int(w.data[i] - '0')
-        currDigit = (currDigit + 1) % 10
-        newWheel := *w
-        newWheel.data[i] = byte(currDigit) + '0'
-        res = append(res, newWheel)
 
-        currDigit = int(w.data[i] - '0')
-        currDigit = currDigit -1
-        if currDigit < 0 {
-            currDigit += 10
+	res := make([]Wheels, 0)
+	for i := 0; i < 4; i++ {
+		currDigit := int(w.data[i] - '0')
+		currDigit = (currDigit + 1) % 10
+		newWheel := NewWheelsFromWheels(*w)
+		newWheel.data[i] = byte(currDigit) + '0'
+		res = append(res, newWheel)
+
+		currDigit = int(w.data[i] - '0')
+		currDigit = currDigit - 1
+		if currDigit < 0 {
+			currDigit += 10
+		}
+
+		newWheel = NewWheelsFromWheels(*w)
+		newWheel.data[i] = byte(currDigit) + '0'
+		res = append(res, newWheel)
+	}
+	return res
+}
+
+func openLock(deadends []string, target string) int {
+	ignore := make(map[Wheels]bool)
+
+	for _, d := range deadends {
+		w := NewWheels(d)
+		ignore[w] = true
+	}
+
+	visited := make(map[Wheels]bool)
+	s := NewWheels("0000")
+	t := NewWheels(target)
+	q := NewMinHeap[Vertex](func(a, b Vertex) bool {
+        if a.c == b.c {
+            return a.getEstCost(t) < b.getEstCost(t)
         }
+		return a.c < b.c
+	},
+		Vertex{s, 0},
+	)
 
-        newWheel = *w
-        newWheel.data[i] = byte(currDigit) + '0'
-        res = append(res, newWheel)
-    }
-    return res
+	if ignore[t] || ignore[s] {
+		return -1
+	}
+
+	var node Vertex
+	for q.Len() > 0 {
+		node = heap.Pop(q).(Vertex)
+
+		if visited[node.w] {
+			continue
+		}
+
+		visited[node.w] = true
+		if node.w == t {
+			return node.c
+		}
+
+		for _, next := range node.w.getNextSteps() {
+			if !visited[next] && !ignore[next] {
+				heap.Push(q, Vertex{next, node.c + 1})
+			}
+		}
+	}
+	return -1
 }
 
 
-func openLock(deadends []string, target string) int {
-    ignore := make(map[Wheels]bool)
+type MinHeap[T any] struct {
+	data []T
+	less func(a, b T) bool
+}
 
-    for _, d := range deadends {
-        w := NewWheels(d)
-        ignore[w] = true
-    }
+func NewMinHeap[T any](less func(a, b T) bool, data ...T) *MinHeap[T] {
+	res := MinHeap[T]{
+		less: less,
+	}
 
-    visited := make(map[Wheels]bool)
+	if len(data) > 0 {
+		res.data = make([]T, len(data))
+		copy(res.data, data)
+		heap.Init(&res)
+	}
 
-    q := make([]Vertex, 0)
-    q = append(q, Vertex{NewWheels("0000"), 0})
-    
-    t := NewWheels(target)
-    
-    var node Vertex
-    for len(q) > 0 {
-        node = q[0]
-        q = q[1:]
+	return &res
+}
 
-        if visited[node.w] {
-            continue
-        } 
+func (m MinHeap[T]) Less(a, b int) bool {
+	return m.less(m.data[a], m.data[b])
+}
 
-        visited[node.w] = true
-        if node.w == t {
-            return node.c
-        }
+func (m MinHeap[T]) Swap(a, b int) {
+	m.data[a], m.data[b] = m.data[b], m.data[a]
+}
 
-        for _, next := range node.w.getNextSteps() {
-            if !visited[next] && !ignore[next] {
-                fmt.Println(string(next.data))
-                q = append(q, Vertex{next, node.c+1})
-            }
-        }
-    }
-    return -1
+func (m MinHeap[T]) Len() int {
+	return len(m.data)
+}
+
+func (m *MinHeap[T]) Pop() any {
+	l := m.Len()
+	item := m.data[l-1]
+	m.data = m.data[0 : l-1]
+	return item
+}
+
+func (m *MinHeap[T]) Push(a any) {
+	m.data = append(m.data, a.(T))
 }
